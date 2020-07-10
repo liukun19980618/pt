e25f38b1284667c544ccbc670d3ead5ebd33a44f
liukun <1020367832@qq.com>
2020-07-10 14:24:23 +0800

    测试

--- pt-manor/src/main/java/com/pt/ptmanor/PtManorApplication.java
+++ pt-manor/src/main/java/com/pt/ptmanor/PtManorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PtManorApplication {
 
 	//测试
+    //测试
 	public static void main(String[] args) {
 		SpringApplication.run(PtManorApplication.class, args);
 	}
